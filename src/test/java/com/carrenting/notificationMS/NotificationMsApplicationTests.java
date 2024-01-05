df800377f56ff3575730d9b8e8105fce736ef0f3
Jan Felix van Son <41387227+janfelixvs@users.noreply.github.com>
2024-01-05 12:52:35 +0100

    Springboot

---
+++ src/test/java/com/carrenting/notificationMS/NotificationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrenting.notificationMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
