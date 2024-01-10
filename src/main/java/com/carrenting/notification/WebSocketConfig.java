085b21a0024a0e2f3ba844af772e69c0ada52a26
Drolt <thomas_klug@gmx.de>
2024-01-10 14:31:00 +0100

    @EnableFeignClients added, didnt help tho

--- src/main/java/com/carrenting/notification/WebSocketConfig.java
+++ src/main/java/com/carrenting/notification/WebSocketConfig.java
@@ -2,12 +2,16 @@ package com.carrenting.notification;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
 
 
     @Override
@@ -21,4 +25,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/websocket").setAllowedOrigins("http://127.0.0.1:5500").withSockJS();
     }
 
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(); // Erstellt eine CORS-Konfigurationsquelle basierend auf URL
+        CorsConfiguration config = new CorsConfiguration();      // Erstellt ein neues CORS-Konfigurationsobjekt
+        config.setAllowCredentials(true);                        // Ermöglicht das Senden von Anmeldeinformationen
+        config.setAllowedOriginPatterns(Arrays.asList("http://127.0.0.1:5500/", "http://localhost:5500")); // Definiert erlaubte Ursprünge (Frontend-URLs)
+        config.addAllowedHeader("");                            // Erlaubt alle Kopfzeilen
+        config.addAllowedMethod("");                            // Erlaubt alle HTTP-Methoden
+        source.registerCorsConfiguration("/**", config); // Registriert die CORS-Konfiguration für alle Pfade
+        return new CorsFilter(source);                           // Gibt den konfigurierten CORS-Filter zurück
+    }
 }
