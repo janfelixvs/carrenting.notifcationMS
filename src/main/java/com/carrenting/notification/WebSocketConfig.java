92ca3429e82cbd4a74f1b6a2752971ab65c5e0d2
Drolt <thomas_klug@gmx.de>
2024-01-10 13:58:07 +0100

    update websocket

--- src/main/java/com/carrenting/notification/WebSocketConfig.java
+++ src/main/java/com/carrenting/notification/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").setAllowedOrigins("http://127.0.0.1:5501").withSockJS();
+        registry.addEndpoint("/websocket").setAllowedOrigins("http://127.0.0.1:5500").withSockJS();
     }
 
 }
